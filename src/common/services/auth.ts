abf3187d11306e66addd968b8735938ce31076a4
An <140888823+ancJ29@users.noreply.github.com>
2024-09-13 12:00:04 +0700

    chore: hotfix

--- src/common/services/auth.ts
+++ src/common/services/auth.ts
@@ -7,10 +7,6 @@ export async function login(payload: {
   userName: string;
   password: string;
 }) {
-  const APP_CLIENT_ID = Number(import.meta.env.APP_CLIENT_ID || 0);
-  if (!APP_CLIENT_ID || isNaN(APP_CLIENT_ID)) {
-    throw new Error("APP_CLIENT_ID is not defined");
-  }
   const action = APP_ACTIONS.AUTH_LOGIN;
   const res = await callApi(
     {
@@ -18,7 +14,7 @@ export async function login(payload: {
       payload: {
         ...payload,
         password: hashMd5(payload.password),
-        clientId: APP_CLIENT_ID,
+        clientId: 2,
       },
     },
     loginSchema,
