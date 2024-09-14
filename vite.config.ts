6c3b08c88d73ed7c62264d84ccaf6133c531b85b
An <140888823+ancJ29@users.noreply.github.com>
2024-09-14 18:24:54 +0700

    sync@20240914.1824

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import preload from "vite-plugin-preload";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default () => {
   return defineConfig({
-    plugins: [react(), preload()],
+    plugins: [react(), preload(), VitePWA({})],
     server: {
       port: 9950,
     },
